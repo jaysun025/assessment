113f590014f7214f2dd3706f969b3a6657a3e765
tkadima <kadima.t@husky.neu.edu>
2021-01-06 14:19:00 -0500

    nothing works

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let pluseBtn = document.getElementById('pluse')
 let minuseBtn = document.getElementById('minues')
-let numberSlot = []
-let numberTotal = []
+// let numberSlot = []
+// let numberTotal = []
 
-function addNumber(onclick) {
-    let numberTotal = 0;
-    if(numberTotal < 0) {
-        return numberTotal;
-    }
-}
 
-function subNumber(onclick, minues) {
-    
+function addNumber(numberTotal, numberSlot) {
+   return pluseBtn + numberSlot
 }
+ addNumber(numberSlot + numberTotal)
+
+function subNumber(numberTotal, numberSlot) {
+    return minuseBtn - numberSlot
+}
+subNumbers(numberSlot - numberTotal)
 
 pluseBtn.addEventListener('click', addNumber)
 minuseBtn.addEventListener('click', subNumber)
