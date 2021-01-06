2e1dfbb566599c648345efba85ee7da9493d8b2b
tkadima <kadima.t@husky.neu.edu>
2021-01-06 14:59:19 -0500

    finally got console.log on buttons

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ minuseBtn.addEventListener('click', subNumber)
 
 function addNumber(numberSlot, numberTotal) {
    total = numberSlot + numberTotal
+   document.getElementById(numberTotal)
+   console.log('yea')
 }
  addNumber(numberSlot + numberTotal)
 
 function subNumber(numberSlot, numberTotal) {
     total = numberSlot - numberTotal
+    document.getElementById(numberTotal)
+    console.log('no')
 }
-subNumbers(numberSlot - numberTotal)
+
+console.log('fuck me')
