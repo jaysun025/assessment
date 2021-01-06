0672d505e17b9f2a79482c4119635750bd70a0cd
tkadima <kadima.t@husky.neu.edu>
2021-01-06 13:36:22 -0500

    set up html and got some js down

---
+++ app.js
@@ -0,0 +1,18 @@
+let pluseBtn = document.getElementById('pluse')
+let minuseBtn = document.getElementById('minues')
+let numberSlot = []
+let numberTotal = []
+
+function addNumber(onclick) {
+    let numberTotal = 0;
+    if(numberTotal < 0) {
+        return numberTotal;
+    }
+}
+
+function subNumber(onclick, minues) {
+    
+}
+
+pluseBtn.addEventListener('click', addNumber)
+minuseBtn.addEventListener('click', subNumber)
