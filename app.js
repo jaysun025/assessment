95dd180dc09f55bece459fc4a3bc9f12a446fd1a
tkadima <kadima.t@husky.neu.edu>
2021-01-06 23:24:42 -0500

    i didn't commit for a while but i got it

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-let pluseBtn = document.getElementById('pluse')
-let minuseBtn = document.getElementById('minues')
-let numberSlot = 0
+const pluseBtn = document.getElementById('pluse')
+const minuseBtn = document.getElementById('minues')
+const numberSlot = document.getElementById('numberSlot')
+
 let numberTotal = 0
 
 pluseBtn.addEventListener('click', addNumber)
 minuseBtn.addEventListener('click', subNumber)
 
-function addNumber(numberSlot, numberTotal) {
-   (total = numberSlot + numberTotal)
-   (document.getElementById(numberTotal))
-   console.log('yea')
+
+function addNumber() {
+   console.log(numberSlot.value)
+   let fromString = parseInt(numberSlot.value)
+   numberTotal = numberTotal + fromString
+   document.getElementById(numberTotal)
+   console.log(numberTotal)
+   updateCount()
 }
 
 
-function subNumber(numberSlot, numberTotal) {
-   (total = numberSlot - numberTotal)
-   (document.getElementById(numberTotal))
-    console.log('no')
+function subNumber() {
+   let fromString = parseInt(numberSlot.value)
+   numberTotal = numberTotal - fromString
+   document.getElementById(numberTotal)
+   console.log(numberTotal)
+   updateCount()
+}
+
+
+function updateCount(){
+   display.innerText = numberTotal
 }
 
 
